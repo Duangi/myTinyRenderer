8b59ffc7aaaeaff92f5096b05345a13f9be674c4
guohang <309349894@qq.com>
2024-05-14 17:03:02 +0800

    lesson1 fourth attempt

--- main.cpp
+++ main.cpp
@@ -30,12 +30,18 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     
     float k = (y1 - y0) / (float)(x1 - x0);
     float b = y0 - k * x0;
+    float y = y0;
+    float  error = 0;
     for(int x=x0; x<=x1;x += 1){
-        float y = k*x + b;
         if(steep){
             image.set(y,x,color);
         }else{
             image.set(x,y,color);
         }
+        error += k; 
+        if (error>.5) { 
+            y += (y1>y0?1:-1); 
+            error -= 1.; 
+        } 
     }
 }
