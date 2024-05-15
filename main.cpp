ef3666ab27dbf85cc5e0b8f39d40c97bac2ef6d7
duang <309349894@qq.com>
2024-05-15 15:44:46 +0800

    add african_head model

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "tgaimage.h"
 #include <algorithm>
+#include "model.h"
+
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
 
 const TGAColor white = TGAColor({255, 255, 255, 255});
