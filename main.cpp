80825f2390b399107cf504c9c56753c213cf7a52
duang <309349894@qq.com>
2024-05-20 16:25:29 +0800

    try render a triangle

--- main.cpp
+++ main.cpp
@@ -1,31 +1,29 @@
 #include <vector>
 #include <cmath>
 #include "tgaimage.h"
-#include "model.h"
 #include "geometry.h"
-#include <iostream>
+#include <algorithm>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
-Model *model = NULL;
-const int width  = 800;
-const int height = 800;
+const TGAColor green = TGAColor(0,   255, 0,   255);
+const int width  = 200;
+const int height = 200;
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) {
     bool steep = false;
-    if (std::abs(x0-x1)<std::abs(y0-y1)) {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
+    if (std::abs(p0.x-p1.x)<std::abs(p0.y-p1.y)) {
+        std::swap(p0.x, p0.y);
+        std::swap(p1.x, p1.y);
         steep = true;
     }
-    if (x0>x1) {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
+    if (p0.x>p1.x) {
+        std::swap(p0, p1);
     }
 
-    for (int x=x0; x<=x1; x++) {
-        float t = (x-x0)/(float)(x1-x0);
-        int y = y0*(1.-t) + y1*t;
+    for (int x=p0.x; x<=p1.x; x++) {
+        float t = (x-p0.x)/(float)(p1.x-p0.x);
+        int y = p0.y*(1.-t) + p1.y*t;
         if (steep) {
             image.set(y, x, color);
         } else {
@@ -34,29 +32,58 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     }
 }
 
-int main(int argc, char** argv) {
-    if (2==argc) {
-        model = new Model(argv[1]);
-    } else {
-        model = new Model("obj/african_head/african_head.obj");
-    }
-    
-    TGAImage image(width, height, TGAImage::RGB);
-    for (int i=0; i<model->nfaces(); i++) {
-        std::vector<int> face = model->face(i);
-        for (int j=0; j<3; j++) {
-            Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j+1)%3]);
-            int x0 = (v0.x+1.)*width/2.;
-            int y0 = (v0.y+1.)*height/2.;
-            int x1 = (v1.x+1.)*width/2.;
-            int y1 = (v1.y+1.)*height/2.;
-            line(x0, y0, x1, y1, image, white);
-        }
-    }
+float cross(Vec2i a,Vec2i b){
+    return a.x * b.y - a.y * b.x;
+}
 
-    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-    image.write_tga_file("output.tga");
-    delete model;
-    return 0;
+// P点是否在三角形内
+bool isInsideTriangle(Vec2i *tri, Vec2i P){
+    Vec2i A = tri[0];
+    Vec2i B = tri[1];
+    Vec2i C = tri[2];
+    
+    Vec2i AB = B - A;
+    Vec2i BC = C - B;
+    Vec2i CA = A - C;
+
+    Vec2i AP = P - A;
+    Vec2i BP = P - B;
+    Vec2i CP = P - C;
+
+    float cross1 = cross(AB,AP);
+    float cross2 = cross(BC,BP);
+    float cross3 = cross(CA,CP);
+
+    return (cross1 >= 0 && cross2 >=0 && cross3 >= 0) || (cross1 <= 0 && cross2 <= 0 && cross3 <= 0);
+}
+
+void triangle(Vec2i *pts, TGAImage &image, TGAColor color) { 
+    // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!) 
+    Vec2i bboxmin(image.get_width()-1,  image.get_height()-1); 
+    Vec2i bboxmax(0, 0); 
+    Vec2i clamp(image.get_width()-1, image.get_height()-1); 
+    
+    for (int i=0; i<3; i++) { 
+        bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
+        bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
+
+        bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
+        bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
+    } 
+    Vec2i P; 
+    for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) { 
+        for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) { 
+            if (!isInsideTriangle(pts,P)) continue; 
+            image.set(P.x, P.y, color); 
+        } 
+    } 
+}
+
+int main(int argc, char** argv) {
+    TGAImage frame(200, 200, TGAImage::RGB); 
+    Vec2i pts[3] = {Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160)}; 
+    triangle(pts, frame, red); 
+    frame.flip_vertically(); // to place the origin in the bottom left corner of the image 
+    frame.write_tga_file("framebuffer.tga");
+    return 0; 
 }
