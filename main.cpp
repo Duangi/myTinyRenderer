78471c620748aae2915ea7f4c43657d2b7f7c9c3
duang <309349894@qq.com>
2024-05-21 10:03:48 +0800

    背面剔除

--- main.cpp
+++ main.cpp
@@ -88,14 +88,22 @@ int main(int argc, char** argv) {
     } else {
         model = new Model("obj/african_head.obj");
     }
-    for (int i=0; i<model->nfaces(); i++) { 
-        std::vector<int> face = model->face(i); 
-        Vec2i screen_coords[3]; 
-        for (int j=0; j<3; j++) { 
-            Vec3f world_coords = model->vert(face[j]); 
-            screen_coords[j] = Vec2i((world_coords.x+1.)*width/2., (world_coords.y+1.)*height/2.); 
-        } 
-        triangle(screen_coords, frame, TGAColor(rand()%255, rand()%255, rand()%255, 255)); 
+    Vec3f light_dir(0,0,-1);
+    for (int i=0; i<model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        Vec2i screen_coords[3];
+        Vec3f world_coords[3];
+        for (int j=0; j<3; j++) {
+            Vec3f v = model->vert(face[j]);
+            screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.);
+            world_coords[j]  = v;
+        }
+        Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
+        n.normalize();
+        float intensity = n*light_dir;
+        if (intensity>0) {
+            triangle(screen_coords, frame, TGAColor(intensity*255, intensity*255, intensity*255, 255));
+        }
     }
     frame.flip_vertically();
     frame.write_tga_file("framebuffer.tga");
