3b486d1f86c063833febca4e77e26a92e493ea9e
duang <309349894@qq.com>
2024-05-23 16:11:34 +0800

    生成根据光的方向变亮度的头，加上了z-buffer，但是边缘还有奇怪的亮点

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <vector>
 #include <cmath>
+#include <cstdlib>
+#include <limits>
 #include "tgaimage.h"
+#include "model.h"
 #include "geometry.h"
 #include <algorithm>
-#include "model.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
@@ -38,6 +40,11 @@ float cross(Vec2i a,Vec2i b){
     return a.x * b.y - a.y * b.x;
 }
 
+Vec3f cross(Vec3f a, Vec3f b){
+    // n = (y1z2 - y2z1, x2z1-z2x1, x1y2 - x2y1)
+    return Vec3f(a.y * b.z - b.y * a.z, b.x * a.z - b.z * a.x, a.x * b.y - b.x * a.y);
+}
+
 // P点是否在三角形内
 bool isInsideTriangle(Vec2i *tri, Vec2i P){
     Vec2i A = tri[0];
@@ -59,26 +66,46 @@ bool isInsideTriangle(Vec2i *tri, Vec2i P){
     return (cross1 >= 0 && cross2 >=0 && cross3 >= 0) || (cross1 <= 0 && cross2 <= 0 && cross3 <= 0);
 }
 
-void triangle(Vec2i *pts, TGAImage &image, TGAColor color) { 
-    // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!) 
-    Vec2i bboxmin(image.get_width()-1,  image.get_height()-1); 
-    Vec2i bboxmax(0, 0); 
-    Vec2i clamp(image.get_width()-1, image.get_height()-1); 
+Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) {
+    Vec3f s[2];
+    for (int i=2; i--; ) {
+        s[i][0] = C[i]-A[i];
+        s[i][1] = B[i]-A[i];
+        s[i][2] = A[i]-P[i];
+    }
+    Vec3f u = cross(s[0], s[1]);
+    if (std::abs(u[2])>1e-2) // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
+        return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
+    return Vec3f(-1,1,1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
+}
+void triangle(Vec3f *pts,float *zbuffer, TGAImage &image, TGAColor color) {  
+    Vec2f bboxmin(image.get_width()-1,  image.get_height()-1); 
+    Vec2f bboxmax(0, 0); 
+    Vec2f clamp(image.get_width()-1, image.get_height()-1); 
     
-    for (int i=0; i<3; i++) { 
-        bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
-        bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
-
-        bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
-        bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
-    } 
-    Vec2i P; 
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<2; j++) {
+            bboxmin[j] = std::max(0.f,      std::min(bboxmin[j], pts[i][j]));
+            bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
+        }
+    }
+    Vec3f P; 
     for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) { 
         for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) { 
-            if (!isInsideTriangle(pts,P)) continue; 
-            image.set(P.x, P.y, color); 
+            Vec3f bc_screen  = barycentric(pts[0], pts[1], pts[2], P);
+            if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0) continue;
+            P.z = 0;
+            for (int i=0; i<3; i++) P.z += pts[i][2]*bc_screen[i];
+            if (zbuffer[int(P.x+P.y*width)]<P.z) {
+                zbuffer[int(P.x+P.y*width)] = P.z;
+                image.set(P.x, P.y, color);
+            }
         } 
     } 
+}
+
+Vec3f world2screen(Vec3f v) {
+    return Vec3f(int((v.x+1.)*width/2.+.5), int((v.y+1.)*height/2.+.5), v.z);
 }
 
 int main(int argc, char** argv) {
@@ -89,21 +116,25 @@ int main(int argc, char** argv) {
         model = new Model("obj/african_head.obj");
     }
     Vec3f light_dir(0,0,-1);
+    float *zbuffer = new float[width*height];
+    for(int i = width*height;i--;zbuffer[i] = -std::numeric_limits<float>::max());
+    
     for (int i=0; i<model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
-        Vec2i screen_coords[3];
-        Vec3f world_coords[3];
-        for (int j=0; j<3; j++) {
-            Vec3f v = model->vert(face[j]);
-            screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.);
-            world_coords[j]  = v;
-        }
-        Vec3f n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
-        n.normalize();
-        float intensity = n*light_dir;
-        if (intensity>0) {
-            triangle(screen_coords, frame, TGAColor(intensity*255, intensity*255, intensity*255, 255));
-        }
+        Vec2i screen_coords[3]; 
+        Vec3f world_coords[3]; 
+        for (int j=0; j<3; j++) { 
+            Vec3f v = model->vert(face[j]); 
+            screen_coords[j] = Vec2i((v.x+1.)*width/2., (v.y+1.)*height/2.); 
+            world_coords[j]  = v; 
+        } 
+        // 
+        Vec3f n = cross(world_coords[2]-world_coords[1],world_coords[1] - world_coords[0]); 
+        n.normalize(); 
+        float intensity = n*light_dir; 
+        Vec3f pts[3];
+        for (int i=0; i<3; i++) pts[i] = world2screen(model->vert(face[i]));
+        triangle(pts, zbuffer, frame, TGAColor(intensity*255, intensity*255, intensity*255, 255));
     }
     frame.flip_vertically();
     frame.write_tga_file("framebuffer.tga");
